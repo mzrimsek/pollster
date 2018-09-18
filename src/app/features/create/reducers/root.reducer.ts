4bad0c3360f732595951088f9ead06a56f500994
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-09-18 08:59:30 -0400

    add selectors

--- src/app/features/create/reducers/root.reducer.ts
+++ src/app/features/create/reducers/root.reducer.ts
@@ -18,9 +18,15 @@ export const _selectCreateState = createFeatureSelector<CreateState>('create');
 export const _selectCreatePoll = createSelector(_selectCreateState, state => state.createPoll);
 
 export const _selectOptions = createSelector(_selectCreatePoll, createPoll => createPoll.options);
+export const _selectTitle = createSelector(_selectCreatePoll, createPoll => createPoll.title);
+export const _selectMode = createSelector(_selectCreatePoll, createPoll => createPoll.selectionMode);
+export const _selectValidUntil = createSelector(_selectCreatePoll, createPoll => createPoll.validUntil);
 
 const createSelectors = {
-  options: _selectOptions
+  options: _selectOptions,
+  title: _selectTitle,
+  mode: _selectMode,
+  validUntil: _selectValidUntil
 };
 
 export default createSelectors;
