f21faebc2ce97e0acf9690ca41a22c69434a5f7c
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-09-21 10:52:48 -0400

    testing complete

--- src/app/features/poll/reducers/root.reducer.spec.ts
+++ src/app/features/poll/reducers/root.reducer.spec.ts
@@ -1,7 +1,33 @@
-// import { PollState, State } from './root.reducer';
+import { _selectVoteInfo, PollState, State } from './root.reducer';
 
-xdescribe('Poll Root Reducer', () => {
+describe('Poll Root Reducer', () => {
   describe('Vote Selectors', () => {
+    describe('_selectVoteInfo', () => {
+      it('Should return dictionary of items', () => {
+        const pollState: PollState = {
+          vote: {
+            ids: ['abcde'],
+            entities: {
+              'abcde': {
+                pollId: 'abcde',
+                option: 'some option',
+                votedOn: 10000
+              }
+            }
+          }
+        };
+        const state: State = { poll: pollState };
 
+        const result = _selectVoteInfo(state);
+
+        expect(result).toEqual({
+          'abcde': {
+            pollId: 'abcde',
+            option: 'some option',
+            votedOn: 10000
+          }
+        });
+      });
+    });
   });
 });
