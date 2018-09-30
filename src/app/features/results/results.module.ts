f55324968973f719699d90b6dbb3d346d8985711
Mike Zrimsek <mikezrimsek@gmail.com>
2018-09-30 14:46:06 -0400

    styled results components
    need to test now

--- src/app/features/results/results.module.ts
+++ src/app/features/results/results.module.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatCardModule, MatListModule } from '@angular/material';
 
 import { ResultsViewComponent } from './components/results-view/results-view.component';
 import { ResultsComponent } from './results.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatListModule
   ],
   declarations: [
     ResultsComponent,
