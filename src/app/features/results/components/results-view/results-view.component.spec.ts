7a0a10fb7fcd7fb62c925125a1c45f077cae2c8f
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-10-12 14:49:16 -0400

    make graph vertical

--- src/app/features/results/components/results-view/results-view.component.spec.ts
+++ src/app/features/results/components/results-view/results-view.component.spec.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule, MatListModule } from '@angular/material';
 
