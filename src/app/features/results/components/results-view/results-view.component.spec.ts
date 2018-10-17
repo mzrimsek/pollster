d9f7709cc687e566bc1a70ec801491897eb11f0e
Mike Zrimsek <mikezrimsek@gmail.com>
2018-10-17 09:14:02 -0400

    Revert "add graph stuff (#25) (#26)"
    This reverts commit faa4e1be87bb891b503bfa4dc11eb49198a54198.

--- src/app/features/results/components/results-view/results-view.component.spec.ts
+++ src/app/features/results/components/results-view/results-view.component.spec.ts
@@ -1,10 +1,7 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule, MatListModule } from '@angular/material';
 
 import { ResultsViewComponent } from './results-view.component';
-
-import { selectColorScheme } from '../../utils/color-scheme.utils';
 
 import { poll } from '../../../../test-helpers';
 
@@ -18,8 +15,7 @@ describe('ResultsViewComponent', () => {
       imports: [
         MatCardModule,
         MatListModule
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      ]
     }).compileComponents();
   }));
 
@@ -27,24 +23,6 @@ describe('ResultsViewComponent', () => {
     fixture = TestBed.createComponent(ResultsViewComponent);
     component = fixture.componentInstance;
     component.poll = poll.testPoll;
-    component.config = {
-      view: undefined,
-      colorScheme: {
-        domain: selectColorScheme('cool')
-      },
-      showLegend: false,
-      gradient: false,
-      animations: true,
-      showXAxis: true,
-      showYAxis: true,
-      showXAxisLabel: false,
-      showYAxisLabel: false,
-      showGridLines: true,
-      xAxisLabel: 'X',
-      yAxisLabel: 'Y',
-      axisTickFormatting: x => x,
-      scaleMax: 5
-    };
     fixture.detectChanges();
   });
 
@@ -52,19 +30,10 @@ describe('ResultsViewComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('getPollResults', () => {
-    it('Should return the list of graph items', () => {
-      const pollResults = component.getPollResults();
-      expect(pollResults).toEqual([{
-        name: 'Chipotle',
-        value: 0
-      }, {
-        name: 'Sheetz',
-        value: 0
-      }, {
-        name: 'Pulp',
-        value: 0
-      }]);
+  describe('getPollOptions', () => {
+    it('Should return the list of properties', () => {
+      const pollOptions = component.getPollOptions();
+      expect(pollOptions).toEqual(['Chipotle', 'Sheetz', 'Pulp']);
     });
   });
 });
